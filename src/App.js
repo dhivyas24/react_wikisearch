873510f32d18b07eb720eb4a5d28f0ed35fda3b6
Dhivya.s <87111672+dhivyas24@users.noreply.github.com>
2022-12-07 22:52:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (search === "") return;
+
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+
+    const response = await fetch(endpoint);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Wiki Search</h1>
+        <form className="search-box" onSubmit={handleSearch}>
+          <input
+            type="search"
+            placeholder="What are you looking for?"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+      </header>
+      <div className="results">
+        {results.map((result, i) => {
+          const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+          return (
+            <div className="result" key={i}>
+              <h3>{result.title}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
